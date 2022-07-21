d74608ecb8b99dff9d13f8af7cf2521eb7f0da01
Aftab <aftab.alam@sabstt.com>
2022-07-21 14:18:59 +0100

    refactored and added multiple charts

--- src/Components/Chart.tsx
+++ src/Components/Chart.tsx
@@ -1,36 +1,53 @@
 import React, { useEffect, useState } from "react";
 
-import {
-  Chart as ChartJS,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Scatter } from "react-chartjs-2";
-
 import moment from "moment";
-
-ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+import { AgeGraph } from "./AgeGraph";
+import { YoeGraph } from "./YoeGraph";
+import { SalaryGraph } from "./SalaryGraph";
 
 const Chart = (props: any) => {
   const [ageData, setAgeData] = useState<any[]>([]);
   const [yoeData, setYoeData] = useState<any[]>([]);
+  const [salaryData, setSalaryData] = useState<any[]>([]);
 
-  let graphAgeData = {
-    datasets: [
-      {
-        label: "Age",
-        data: ageData,
-        backgroundColor: "rgba(255, 99, 132, 1)",
-      },
-    ],
+  let allData = {
+    graphAgeData: {
+      datasets: [
+        {
+          label: "Age",
+          data: ageData,
+          backgroundColor: "rgba(255, 99, 132, 1)",
+        },
+      ],
+    },
+
+    graphYoeData: {
+      datasets: [
+        {
+          label: "Years Of Experience",
+          data: yoeData,
+          backgroundColor: "blue",
+        },
+      ],
+    },
+
+    graphSalaryData: {
+      datasets: [
+        {
+          label: "Years Of Experience",
+          data: salaryData,
+          backgroundColor: "purple",
+        },
+      ],
+    },
   };
-  const ageDataOptions = {
-    scales: {
-      y: {
-        beginAtZero: true,
+
+  const allOptions = {
+    ageDataOptions: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
     },
   };
@@ -47,13 +64,35 @@ const Chart = (props: any) => {
         y: user.years_of_experience,
         x: index,
       });
+
+      salaryData.push({
+        y: user.salary,
+        x: index,
+      });
     });
   }, []);
 
   return (
     <div>
       {ageData.length > 0 && (
-        <Scatter options={ageDataOptions} data={graphAgeData} />
+        <AgeGraph
+          graphData={allData.graphAgeData}
+          dataOptions={allOptions.ageDataOptions}
+        />
+      )}
+
+      {yoeData.length > 0 && (
+        <YoeGraph
+          graphData={allData.graphYoeData}
+          dataOptions={allOptions.ageDataOptions}
+        />
+      )}
+
+      {salaryData.length > 0 && (
+        <SalaryGraph
+          graphData={allData.graphSalaryData}
+          dataOptions={allOptions.ageDataOptions}
+        />
       )}
     </div>
   );
